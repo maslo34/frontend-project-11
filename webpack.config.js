f5a9d2c322a46cf964967433e533d3659f6e5179
maslo34 <pasha.golyov3021@gmail.com>
2024-08-15 23:45:22 +0300

    finishing the environment setup

--- webpack.config.js
+++ webpack.config.js
@@ -26,17 +26,33 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
+              test: /\.(scss)$/,
+              use: [
+                {
+                  loader: 'style-loader'
+                },
+                {
+                  loader: 'css-loader'
+                },
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: () => [
+                        require('autoprefixer')
+                      ]
+                    }
+                  }
+                },
+                {
+                  loader: 'sass-loader'
+                }
+              ]
+            }
+          ]
+        //     // Add your rules for custom modules here
+        //     // Learn more about loaders from https://webpack.js.org/loaders/
+        // ],
     },
 };
 
